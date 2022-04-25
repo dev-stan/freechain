682ee0f7224145a64e4744dda21b6984ffa4061f
dev-stan <mrstanleyzalewski@gmail.com>
2022-04-25 02:51:43 +0200

    .

--- chain/chain/asgi.py
+++ chain/chain/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mysite project.
+ASGI config for chain project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chain.settings')
 
 
 application = get_asgi_application()
